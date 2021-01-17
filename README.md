c886c772acadf6e49df054bef1662f22a01a6173
mwcd <mateo.cohen.davis@gmail.com>
2021-01-17 13:47:20 -0500

    Add README, update how table is generated and its styling

---
+++ README.md
@@ -0,0 +1,11 @@
+# Restock Wizard
+A webapp written in ExpressJS and React that allows you to keep on top of GPU stock and prices.
+
+## Getting Started
+* Download the application
+* Enter the `node-server` directory
+* Run `npm install`
+* Run `npm start`
+* Enter the `client` directory
+* Run `npm install`
+* Run `npm start`
--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { GpuStock } from './Interfaces/interfaces'
+import { GpuInfo, GpuStock } from './Interfaces/interfaces'
 import { Cell, useTable } from 'react-table'
 import axios from 'axios'
 
@@ -24,17 +24,12 @@ export default function Home() {
     axios.get('http://localhost:8080/gpus')
       .then(
         result => {
-          console.log("1")
           setGpus(result.data.gpus)
-          console.log("2")
           setIsLoaded(true)
-          console.log("3")
-          console.log("result: " + result)
         },
         error => {
           setIsLoaded(true)
           setError(error)
-          console.log("error: " + error)
         }
       )
   }, [])
@@ -42,7 +37,9 @@ export default function Home() {
   if (error.isError) {
     return <div>Error: {error.errorBody}</div>;
   } else if (!isLoaded) {
-    return <Loading />;
+    return (
+      <Loading />
+    );
   } else {
     return (
       <HomeTable gpus={gpus} />
@@ -62,50 +59,15 @@ interface HomeTableProps {
 
 function HomeTable({ gpus }: HomeTableProps) {
   const data = React.useMemo(
-    () => [
-      {
-        prodName: 'RTX 3060 Ti',
-        avail: gpus.nvidia3060Ti[0].inStock ? 'Available' : 'Not Available',
-        price: gpus.nvidia3060Ti[0].price,
-        link: gpus.nvidia3060Ti[0].address
-      },
-      {
-        prodName: 'RTX 3070',
-        avail: gpus.nvidia3070[0].inStock ? 'Available' : 'Not Available',
-        price: gpus.nvidia3070[0].price,
-        link: gpus.nvidia3070[0].address
-      },
-      {
-        prodName: 'RTX 3080',
-        avail: gpus.nvidia3080[0].inStock ? 'Available' : 'Not Available',
-        price: gpus.nvidia3080[0].price,
-        link: gpus.nvidia3080[0].address
-      },
-      {
-        prodName: 'RTX 3090',
-        avail: gpus.nvidia3090[0].inStock ? 'Available' : 'Not Available',
-        price: gpus.nvidia3090[0].price,
-        link: gpus.nvidia3090[0].address
-      },
-      {
-        prodName: 'RX 6800',
-        avail: gpus.amdRx6800[0].inStock ? 'Available' : 'Not Available',
-        price: gpus.amdRx6800[0].price,
-        link: gpus.amdRx6800[0].address
-      },
-      {
-        prodName: 'RX 6800 XT',
-        avail: gpus.amdRx6800Xt[0].inStock ? 'Available' : 'Not Available',
-        price: gpus.amdRx6800Xt[0].price,
-        link: gpus.amdRx6800Xt[0].address
-      },
-      {
-        prodName: 'RX 6900',
-        avail: gpus.amdRx6900Xt[0].inStock ? 'Available' : 'Not Available',
-        price: gpus.amdRx6900Xt[0].price,
-        link: gpus.amdRx6900Xt[0].address
-      },
-    ],
+    () => {
+      let homePageGpus: GpuInfo[] = []
+      for (let key in gpus) {
+        if (gpus.hasOwnProperty(key)) {
+          homePageGpus.push((gpus as any)[key][0])
+        }
+      }
+      return homePageGpus
+    },
     [gpus]
   )
 
@@ -113,11 +75,12 @@ function HomeTable({ gpus }: HomeTableProps) {
     () => [
       {
         Header: 'Product Name',
-        accessor: 'prodName',
+        accessor: 'name',
       },
       {
         Header: 'Availability',
-        accessor: 'avail',
+        accessor: 'inStock',
+        Cell: (cell: Cell) => cell.value ? 'Available' : 'Unavailable'
       },
       {
         Header: 'Price',
@@ -125,7 +88,7 @@ function HomeTable({ gpus }: HomeTableProps) {
       },
       {
         Header: 'Link',
-        accessor: 'link',
+        accessor: 'address',
         Cell: (cell: Cell) => <a target="_blank" rel="noopener noreferrer" href={cell.value}>
           View &rarr;</a>
       },
@@ -144,7 +107,7 @@ function HomeTable({ gpus }: HomeTableProps) {
   } = tableInstance
 
   return (
-    <table {...getTableProps()}>
+    <table className='homeTable' {...getTableProps()}>
       <thead>
         {headerGroups.map(headerGroup => (
           <tr {...headerGroup.getHeaderGroupProps()}>
