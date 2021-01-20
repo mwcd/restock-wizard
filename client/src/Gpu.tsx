89879344dc58e903002cf00fdc358c994dc964d6
mwcd <mateo.cohen.davis@gmail.com>
2021-01-19 23:46:26 -0500

    Make all other Gpus use new GpuTable component

--- client/src/Gpu.tsx
+++ client/src/Gpu.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react'
 import { GpuInfo, GpuType } from './Interfaces/interfaces'
-import { Cell, useTable } from 'react-table'
 import axios from 'axios'
+import GpuTable from './GpuTable'
 
 interface props {
   gpuType: GpuType
@@ -45,113 +45,5 @@ export default function Gpu({ gpuType }: props) {
 function Loading() {
   return (
     <div>Loading...</div>
-  )
-}
-
-interface GpuTableProps {
-  gpus: GpuInfo[]
-}
-
-function GpuTable({ gpus }: GpuTableProps) {
-  let data: GpuInfo[]
-  data = React.useMemo(
-    () => gpus,
-    [gpus]
-  )
-
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Product Name',
-        accessor: 'gpuType'
-      },
-      {
-        Header: '',
-        accessor: 'name',
-      },
-      {
-        Header: 'Availability',
-        accessor: 'inStock',
-        Cell: (cell: Cell) => cell.value ? 'Available' : 'Unavailable'
-      },
-      {
-        Header: 'Price',
-        accessor: 'price',
-      },
-      {
-        Header: 'Link',
-        accessor: 'address',
-        Cell: (cell: Cell) => <a target="_blank" rel="noopener noreferrer" href={cell.value}>
-          View &rarr;</a>
-      },
-    ],
-    []
-  )
-  // @ts-ignore
-  const tableInstance = useTable({ columns, data })
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = tableInstance
-
-  return (
-    <table className='gpuTable' {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => {
-              if (column.Header === 'Product Name') {
-                return (
-                  <th {...column.getHeaderProps()} colSpan={2}
-                  >
-                    {column.render('Header')}
-                  </th>
-                )
-              } else if (column.Header === '') {
-                return (
-                  <Fragment></Fragment>
-                )
-              } else {
-                return (
-                  <th
-                    {...column.getHeaderProps()}
-                  >
-                    {column.render('Header')}
-                  </th>
-                )
-              }
-            })}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                if (cell.column.id === 'name') {
-                  return (
-                    <td {...cell.getCellProps()} >
-                      {cell.render('Cell')}
-                    </td>
-                  )
-                } else {
-                  return (
-                    <td {...cell.getCellProps()} className='noWrap' >
-                      {cell.render('Cell')}
-                    </td>
-                  )
-                }
-              })}
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
   )
 }
